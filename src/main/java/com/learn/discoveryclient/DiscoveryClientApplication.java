92b13b25b75ce06e93c23bce90d6a96ea3c9af36
com-learn <midoshkm@hotmail.com>
2021-07-19 23:50:56 +0530

    feign client

--- src/main/java/com/learn/discoveryclient/DiscoveryClientApplication.java
+++ src/main/java/com/learn/discoveryclient/DiscoveryClientApplication.java
@@ -7,15 +7,19 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 public class DiscoveryClientApplication {
 
+	
 	@Bean
 	RestTemplate restTemplate(){
 		return new RestTemplate();
@@ -23,9 +27,18 @@ public class DiscoveryClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryClientApplication.class, args);
+		
 	}
 
 	
+}
+
+
+
+@FeignClient(name = "bootdemo")
+interface Bootclient {
+    @GetMapping("/api/bootdemo/hello")
+    String getHello();
 }
 
 @RestController
@@ -34,19 +47,24 @@ class DiscoveryController {
 
     @Autowired 
     RestTemplate restTemplate;
+
+	@Autowired
+	Bootclient bootclient;
+
 	@GetMapping("/hello")
 	public String sayHello() {
 
-        return "Hello Discovery";
+        return "Hello Discovery! \n";
 	}
     @GetMapping("/callboot")
 	public String callHello() {
 
-        String url = "http://bootdemo:8080/api/bootdemo/hello";
-        ResponseEntity<String> entity=
-         restTemplate.getForEntity(url, String.class);
+        //String url = "http://bootdemo:8080/api/bootdemo/hello";
+        //ResponseEntity<String> entity=
+         //restTemplate.getForEntity(url, String.class);
 
-         return "Calling bootdemo from discovery: "+entity.getBody();
+         //return "Calling bootdemo from discovery: "+entity.getBody()+"\n";
+		 return "Calling bootdmo from discovery: "+bootclient.getHello();
     }
     
   
