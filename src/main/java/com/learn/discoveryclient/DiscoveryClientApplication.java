08ca37af1a3de75000c8e170a77ca0b1857bd230
com-learn <midoshkm@hotmail.com>
2021-07-20 23:53:12 +0530

    Circuit Breaker

--- src/main/java/com/learn/discoveryclient/DiscoveryClientApplication.java
+++ src/main/java/com/learn/discoveryclient/DiscoveryClientApplication.java
@@ -2,39 +2,27 @@ package com.learn.discoveryclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
-
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreaker;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.beans.factory.annotation.Autowired;
 //@EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
 public class DiscoveryClientApplication {
 
-	
-	@Bean
-	RestTemplate restTemplate(){
-		return new RestTemplate();
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryClientApplication.class, args);
 		
 	}
 
-	
 }
-
-
-
 @FeignClient(name = "bootdemo")
 interface Bootclient {
     @GetMapping("/api/bootdemo/hello")
@@ -45,12 +33,12 @@ interface Bootclient {
 @RequestMapping("/api/discovery")
 class DiscoveryController {
 
-    @Autowired 
-    RestTemplate restTemplate;
-
 	@Autowired
 	Bootclient bootclient;
-
+    
+	@Autowired
+	Resilience4JCircuitBreakerFactory circuitBreakerFactory;
+    
 	@GetMapping("/hello")
 	public String sayHello() {
 
@@ -59,13 +47,16 @@ class DiscoveryController {
     @GetMapping("/callboot")
 	public String callHello() {
 
-        //String url = "http://bootdemo:8080/api/bootdemo/hello";
-        //ResponseEntity<String> entity=
-         //restTemplate.getForEntity(url, String.class);
-
-         //return "Calling bootdemo from discovery: "+entity.getBody()+"\n";
-		 return "Calling bootdmo from discovery: "+bootclient.getHello();
+        Resilience4JCircuitBreaker circuitBreaker = circuitBreakerFactory.create("inventory");
+        
+		String bootHello = 
+		circuitBreaker.run(
+			() -> { return bootclient.getHello();}, 
+			throwable -> fallbackHello());
+		 return "Calling bootdmo from discovery: "+ bootHello;
     }
     
-  
+	private String fallbackHello(){
+		return "Bootdemo unavailable; please try again later \n";
+	}
 }
